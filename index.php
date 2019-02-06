c4e6b1e6fa9dd6310edd049d789e73c2ba2dc1a0
jnlucas <joao.lucas@duosystem.com.br>
2019-02-06 16:01:08 -0200

    criando funcoes e formulario html

--- index.php
+++ index.php
@@ -1,14 +1,93 @@
-<?php 
+<?php
+header('Content-type: text/html; charset=utf-8');
 ini_set("display_errors",1);
+$variavel = "asas";
+//refinindo variaveis
+$resultado = 80;
 
-error_reporting(E_ALL);
+//criando funcoes
+function somar($a,$b){
 
-$variavel = null;
+	 $resultado = $a + $b;
 
-var_dump($variavel);
+	return $resultado;
+}
 
-$variavel = "asasas";
+// chamando funcoes
+ somar(12,14);
 
-var_dump($variavel);
+
+
+// +
+// -
+// *
+// /
+// %
+
+// >
+// <
+// >=
+// <=
+// ==  1==2
+// =
+// ===
+// !=
+// !==
+
+
+
+
+ function media($nota ){
+ 	if($nota > 5){
+ 		$nota = "aprovado";
+ 	}elseif($nota === 5){
+ 		$nota = "é exatamente a $nota";
+ 	}elseif($nota == 5){
+ 		$nota = "é somente $nota";
+ 	}else{
+ 		$nota = "nenhuma das anteriores";
+ 	}
+
+ 	return $nota;
+
+ }
+
+
+
+
+  //media(5);
+
+
+
+$valorDoFormulario = @$_POST['media'];
+
+
+
+
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>pagina inicial</title>
+	<meta />
+</head>
+<body>
+
+<form action="index.php" method="post">
+	
+<input type="text" value="" name="media"></input>
+
+<input type="submit" value="enviar"></input>
+</form>
+<?php echo media($valorDoFormulario);?>
+
+</body>
+</html>
+
+
+
+
+
+ 
